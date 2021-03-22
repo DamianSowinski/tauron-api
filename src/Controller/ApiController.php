d41ed2aa2b5b33ff9528b4a069090291ded940a2
Damian <damian.sowinski@hotmail.com>
2021-03-22 09:57:28 +0100

    Add api controller

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api", name="api")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+}
